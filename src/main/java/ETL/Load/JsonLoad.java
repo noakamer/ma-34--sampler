ca9bed992dc26149ca6368a0615dc4ee790d6937
Kamernoa <kamernoa@gmail.com>
2022-01-16 18:11:04 +0200

    changed jsonload constractor

--- src/main/java/ETL/Load/JsonLoad.java
+++ src/main/java/ETL/Load/JsonLoad.java
@@ -1,20 +1,23 @@
 package ETL.Load;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class JsonLoad implements Load{
+public class JsonLoad implements Load {
     private ObjectMapper mapper;
 
-    public JsonLoad(){
-        mapper=new ObjectMapper();
+    public JsonLoad() {
+        mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
     }
-    
+
     @Override
     public void load(String path, List list) throws IOException {
-        mapper.writeValue(new File(path),list);
+        mapper.writeValue(new File(path), list);
     }
 }
