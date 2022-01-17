00ab2112d33cfec1c4145fa8bf77a699b87654c5
Kamernoa <kamernoa@gmail.com>
2022-01-17 13:44:39 +0200

    created etlManager abstract class

--- src/main/java/ETL/ETLManager.java
+++ src/main/java/ETL/ETLManager.java
@@ -1,4 +1,33 @@
 package ETL;
 
-public class ETLManager {
+import ETL.Extract.CsvExtract;
+import ETL.Extract.Extract;
+import ETL.Extract.Parse.CsvParser;
+import ETL.Extract.Parse.Parse;
+import ETL.Load.JsonLoad;
+import ETL.Load.Load;
+import ETL.Transform.CastCsvRecordListToStringList;
+import ETL.Transform.ChangePath;
+import ETL.Transform.Split50000;
+
+import java.io.IOException;
+
+public abstract class ETLManager {
+    protected Extract extract;
+    protected Parse parse;
+    protected CastCsvRecordListToStringList castToStringList;
+    protected Split50000 splitClass;
+    protected Load load;
+    protected ChangePath change;
+
+    public ETLManager() {
+        extract = new CsvExtract();
+        parse = new CsvParser();
+        castToStringList = new CastCsvRecordListToStringList();
+        splitClass = new Split50000();
+        load = new JsonLoad();
+        change = new ChangePath();
+    }
+
+    public abstract void manager(String path) throws IOException;
 }
--- src/main/java/MadaReports/MadaReportsManager.java
+++ src/main/java/MadaReports/MadaReportsManager.java
@@ -1,5 +1,6 @@
 package MadaReports;
 
+import ETL.ETLManager;
 import ETL.Extract.CsvExtract;
 import ETL.Extract.Extract;
 import ETL.Extract.Parse.CsvParser;
@@ -13,23 +14,16 @@ import ETL.Transform.Split50000;
 import java.io.IOException;
 import java.util.List;
 
-public class MadaReportsManager {
+public class MadaReportsManager extends ETLManager {
 
-    public void manager (String path) throws IOException {
-        Extract extract = new CsvExtract();
-        Parse parse = new CsvParser();
-        CastCsvRecordListToStringList castToStringList = new CastCsvRecordListToStringList();
+    public void manager(String path) throws IOException {
         MadaReportsList madaReportsListClass = new MadaReportsList();
-        Split50000 splitClass = new Split50000();
-        Load load = new JsonLoad();
-        ChangePath change = new ChangePath();
-
         List<MadaReports> madaReportsList = madaReportsListClass.stringListToObjectList(castToStringList.CsvRecordToString(parse.parse(extract.read(path))));
         List<List<MadaReports>> splitReports = splitClass.split50000(madaReportsList);
         String basicPath = "C:\\Users\\kamer\\Desktop\\intellijProjects\\mada_reports\\";
-        for (List<MadaReports> list:splitReports) {
+        for (List<MadaReports> list : splitReports) {
             basicPath = change.ChangePath(basicPath);
-            load.load(basicPath,list);
+            load.load(basicPath, list);
         }
     }
 
