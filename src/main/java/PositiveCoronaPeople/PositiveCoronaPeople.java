322ef2a00dade29896074c7c81782238325a8b6b
Kamernoa <kamernoa@gmail.com>
2022-01-17 19:14:49 +0200

    added PositiveCoronaPeople and PositiveCoronaPeopleManager classes

---
+++ src/main/java/PositiveCoronaPeople/PositiveCoronaPeople.java
@@ -0,0 +1,25 @@
+package PositiveCoronaPeople;
+
+import lombok.Getter;
+
+public class PositiveCoronaPeople {
+    @Getter private int IDNum;
+    private int IDType;
+    private String FirstName;
+    private String LastName;
+    private String City;
+    private String Street;
+    private int BuildingNumber;
+    private String Barcode;
+    private String BirthDate;
+    private String LabCode;
+    private String ResultDate;
+    private String TakeDate;
+    private String StickerNumber;
+    private int ResultTestCorona;
+    private String Variant;
+    private String TestType;
+
+    public PositiveCoronaPeople(){}
+
+}
