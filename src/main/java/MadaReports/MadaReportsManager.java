49e42998d8b8b6371b44050c97ccb4492539567b
Kamernoa <kamernoa@gmail.com>
2022-01-17 13:46:51 +0200

    deleted etlmanager abstract class

--- src/main/java/MadaReports/MadaReportsManager.java
+++ src/main/java/MadaReports/MadaReportsManager.java
@@ -1,6 +1,5 @@
 package MadaReports;
 
-import ETL.ETLManager;
 import ETL.Extract.CsvExtract;
 import ETL.Extract.Extract;
 import ETL.Extract.Parse.CsvParser;
@@ -14,10 +13,17 @@ import ETL.Transform.Split50000;
 import java.io.IOException;
 import java.util.List;
 
-public class MadaReportsManager extends ETLManager {
+public class MadaReportsManager{
 
     public void manager(String path) throws IOException {
+        Extract extract = new CsvExtract();
+        Parse parse = new CsvParser();
+        CastCsvRecordListToStringList castToStringList = new CastCsvRecordListToStringList();
         MadaReportsList madaReportsListClass = new MadaReportsList();
+        Split50000 splitClass = new Split50000();
+        Load load = new JsonLoad();
+        ChangePath change = new ChangePath();
+
         List<MadaReports> madaReportsList = madaReportsListClass.stringListToObjectList(castToStringList.CsvRecordToString(parse.parse(extract.read(path))));
         List<List<MadaReports>> splitReports = splitClass.split50000(madaReportsList);
         String basicPath = "C:\\Users\\kamer\\Desktop\\intellijProjects\\mada_reports\\";
